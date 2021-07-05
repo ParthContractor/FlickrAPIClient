357b8c1d26e3ac0cb9d9023e8fe55225947b3dab
Parth Contractor <contractor110@gmail.com>
2021-07-06 03:24:01 +1000

    minor fix

--- Sources/FlickrAPIClient/FlickrAPIClient.swift
+++ Sources/FlickrAPIClient/FlickrAPIClient.swift
@@ -9,7 +9,7 @@ final public class FlickrPhotosSearchClient: LoadJSONData {
     /// Page starts from 1 and default limit set is 25. Keep calling for more records with same page number until total number of photos for that page reached
     /// track page number and total at presentation/app layer
     public func getPhotos(searchText: String, page: Int) -> FlickrResponseModel? {
-        if let url = URL(string: str_API_Base_URL + "&api_key=\(str_API_Key)" + "&per_page=\(per_Page_Limit)" + "&text=\(searchText)" + "&page=\(page)" + "&method=\(methodName)") {
+        if let url = URL(string: str_API_Base_URL + "&api_key=\(str_API_Key)" + "&per_page=\(per_Page_Limit)" + "&text=\(searchText)" + "&page=\(page)" + "&method=\(methodName.rawValue)") {
             return self.loadJsonData(url)
         }
         return nil
