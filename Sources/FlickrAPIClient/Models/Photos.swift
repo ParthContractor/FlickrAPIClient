8a5b6f3f8f54f007d3a9517eed551d929a9f68e8
Parth Contractor <contractor110@gmail.com>
2021-07-07 15:45:11 +1000

    pages should be public for consumer to know total pages in the given results set

--- Sources/FlickrAPIClient/Models/Photos.swift
+++ Sources/FlickrAPIClient/Models/Photos.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public struct Photos: Decodable {
     let page: Int64
-    let pages: Int64
+    public let pages: Int64
     let perpage: Int64
     public let total: Int64
     public let photo: [Photo]?
